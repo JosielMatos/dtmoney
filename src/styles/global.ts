17ccd525d4dc71bee07cb2fae92854b44d00e715
JosielMatos <josielmatos77@protonmail.com>
2021-11-10 22:19:22 -0300

    add summary component

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,6 +5,7 @@ export const GlobalStyle = createGlobalStyle`
     --background: #f0f2f5;
     --red: #e5e4ad;
     --blue: #5429cc;
+    --green: #33cc95;
     --blue-light: #6933ff;
     --shape: #fff;
     --text-title: #363f5f;
