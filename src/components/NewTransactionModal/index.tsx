d8cd786067ae8cb2f24c1842fa72eded71a19d7f
JosielMatos <josielmatos77@protonmail.com>
2021-11-12 11:47:12 -0300

    add react-modal form & styles

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,32 @@
+import Modal from "react-modal";
+import closeImg from '../../assets/close.svg';
+import { Container } from "./styles";
+
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button type="button" onClick={onRequestClose} className="modal-close-button">
+        <img src={closeImg} alt="Close modal" />
+      </button>
+
+      <Container>
+        <h2>Register Transaction</h2>
+
+        <input placeholder="Title" />
+        <input type="number" placeholder="Amount" />
+        <input placeholder="Category" />
+        <button type="submit">Submit</button>
+      </Container>
+    </Modal>
+  );
+}
