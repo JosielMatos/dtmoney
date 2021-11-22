cb2dd8d2cb0d85f09d943b0827577fc60d3eba6d
JosielMatos <josielmatos77@protonmail.com>
2021-11-22 17:46:26 -0300

    new transaction api request working

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,40 +1,38 @@
-import { useContext } from 'react';
-import incomeImg from '../../assets/income.svg';
-import outlayImg from '../../assets/outlay.svg';
-import totalImg from '../../assets/total.svg';
-import { TransactionsContext } from '../../TransactionsContext';
+import { useContext } from "react";
+import incomeImg from "../../assets/income.svg";
+import outlayImg from "../../assets/outlay.svg";
+import totalImg from "../../assets/total.svg";
+import { TransactionsContext } from "../../TransactionsContext";
 
 import { Container } from "./styles";
 
 export function Summary() {
-  const data = useContext(TransactionsContext);
-
-  console.log(data)
+  const { transactions } = useContext(TransactionsContext);
 
   return (
     <Container>
       <div>
         <header>
           <p>Income</p>
-          <img src={incomeImg} alt="Income" />
+          <img src={incomeImg} alt='Income' />
         </header>
-          <strong>$4000,00</strong>
+        <strong>$4000,00</strong>
       </div>
 
       <div>
         <header>
           <p>Expenses</p>
-          <img src={outlayImg} alt="Outlay" />
+          <img src={outlayImg} alt='Outlay' />
         </header>
-          <strong>- $1100,00</strong>
+        <strong>- $1100,00</strong>
       </div>
 
-      <div className="highlight-background">
+      <div className='highlight-background'>
         <header>
           <p>Balance</p>
-          <img src={totalImg} alt="Total" />
+          <img src={totalImg} alt='Total' />
         </header>
-          <strong>$2900,00</strong>
+        <strong>$2900,00</strong>
       </div>
     </Container>
   );
