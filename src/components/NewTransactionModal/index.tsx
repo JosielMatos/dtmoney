d9b2de96699fbd9607cf990134badc471630e1b6
JosielMatos <josielmatos77@protonmail.com>
2021-11-23 15:28:52 -0300

    Update hook import

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,12 +1,11 @@
 import Modal from "react-modal";
-import { FormEvent, useContext, useState } from "react";
+import { FormEvent, useState } from "react";
 import { Container, NewTransactionTypeContainer, TypeBox } from "./styles";
-
-import { TransactionsContext } from "../../TransactionsContext";
 
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outlayImg from "../../assets/outlay.svg";
+import { useTransactions } from "../../hooks/useTransactions";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -17,7 +16,7 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
-  const { createTransaction } = useContext(TransactionsContext);
+  const { createTransaction } = useTransactions();
 
   const [title, setTitle] = useState("");
   const [amount, setAmount] = useState(0);
